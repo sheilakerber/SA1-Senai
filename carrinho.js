4d3e3ebdf8e5b535971dd2c3fd0eb7e3472f9012
Sheila Kerber <sheila.skerber@gmail.com>
2020-10-27 21:35:21 -0300

    cria funcionalidades para salvar a lista no local storage e gerar página de relatório

--- carrinho.js
+++ carrinho.js
@@ -67,8 +67,10 @@ function Carrinho() {
             let textSubtotal = document.createElement('div')
             textSubtotal.setAttribute('id', `ST${arrayObjetosSelecionados.indexOf(compra)}`)
             cellSubtotal.appendChild(textSubtotal)
+            cellSubtotal.setAttribute('name', "nomeCellSubtotal")
             textSubtotal.innerText = compra.Valor
 
+            idRow = arrayObjetosSelecionados.indexOf(compra)
         }
     }
 
@@ -94,14 +96,16 @@ function dynamicSort(property) {
     }
 }
 
+var totalListaLS
 
 function calcular() {
     var listaCarrinho = JSON.parse(localStorage.getItem("ListaFinal"))
     listaCarrinho.sort(dynamicSort("Produto"))
     var total = 0
 
-    for(i=0; i<listaCarrinho.length; i++) {
+    for (i = 0; i < listaCarrinho.length; i++) {
         var qtde = document.getElementById(i).value
+            //console.log('qtdes', qtde)
         var subTotal = listaCarrinho[i].Valor * qtde
 
         var sub = document.getElementById(`ST${i}`)
@@ -111,12 +115,120 @@ function calcular() {
 
     let dataCarrinho = ["", "", "", "", "", "TOTAL", parseFloat(total.toFixed(2))]
 
-    let thead = table.createTHead();
+    thead = table.createTHead();
     let row = thead.insertRow();
     for (let key of dataCarrinho) {
         let th = document.createElement("th");
         let text = document.createTextNode(key);
         th.appendChild(text);
         row.appendChild(th);
+    }
+
+    totalListaLS = total
+
+}
+
+function salvarListaLS() {
+    //limpar html de confirmacao de cpf
+    document.getElementById("confirmaCPF").innerHTML = " "
+
+    //solicitar confirmacao de cpf
+    var cpfValidar = document.getElementById("confirmaCPF")
+
+    //cria texto no html
+    var htmlConfirmaCpf = document.createElement('p');
+    cpfValidar.appendChild(htmlConfirmaCpf)
+    htmlConfirmaCpf.innerHTML = "Confirme seu CPF"
+
+    //cria input para inserir cpf
+    var confirmaCpf = document.createElement("input");
+    confirmaCpf.setAttribute('type', 'number');
+    confirmaCpf.setAttribute('id', 'idConfirmaCpf');
+    cpfValidar.appendChild(confirmaCpf)
+
+    //cria botao confirmar
+    var botaoConfirmaCpf = document.createElement("button");
+    cpfValidar.appendChild(botaoConfirmaCpf)
+    botaoConfirmaCpf.innerHTML = "Confirmar"
+    botaoConfirmaCpf.addEventListener('click', confirmeCpf)
+
+
+    //array para salvar listas no LS
+    var listasLocalStorage = []
+
+    function confirmeCpf() {
+        //div para validacao
+        let divConfirmacaoCpf = document.createElement('div')
+        divConfirmacaoCpf.setAttribute('id', 'confirmarCpf')
+        confirmaCPF.appendChild(divConfirmacaoCpf)
+
+        //pegar div de confirmacao
+        var divConfirmacao = document.getElementById("confirmarCpf")
+
+        //pegar o valor
+        var confirmaCpf = document.getElementById("idConfirmaCpf")
+
+        //pegar dados do local storage
+        var listaCpf = JSON.parse(localStorage.getItem('usuários'))
+
+        var confirmar
+
+        for (var i = 0; i < listaCpf.length; i++) {
+            if (confirmaCpf.value == listaCpf[i].cpf) {
+                confirmar = true
+            }
+        }
+
+        var relatoriosSalvos = []
+        var relatorioUsuario = {}
+
+
+        if (confirmar) {
+            document.getElementById("idConfirmaCpf").value = ""
+            window.alert('Cpf Correto!')
+            document.getElementById("idConfirmaCpf").value = ""
+
+            //funcao para salvar a tabela a ser enviada para o LS
+            var tableRelatorio = document.getElementById("tabelaCarrinho");
+
+            for (i = 1; i < tableRelatorio.rows.length; i++) {
+                var qtidade = document.getElementById(i - 1)
+                var subtotal = document.getElementById(`ST${i - 1}`)
+
+                // console.log("qtidade ", qtidade.value);
+                // console.log("subtotal ", subtotal.innerText);
+                // console.log("i ", i - 1);
+
+                var tempListaLS = {}
+
+                tempListaLS.Produto = tableRelatorio.rows[i].cells[0].innerText
+                tempListaLS.Marca = tableRelatorio.rows[i].cells[1].innerText
+                tempListaLS.PesoVolume = tableRelatorio.rows[i].cells[2].innerText
+                tempListaLS.Estabelecimento = tableRelatorio.rows[i].cells[3].innerText
+                tempListaLS.Valor = tableRelatorio.rows[i].cells[4].innerText
+                    //tempListaLS.Quantidade = qtidade.value
+                    //tempListaLS.SubTotal = subtotal.innerText
+            }
+
+            var now = new Date
+            var diaAtual = `${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()}`
+
+            relatorioUsuario.cpfUsuario = document.getElementById("idConfirmaCpf").value
+            relatorioUsuario.dataLista = diaAtual
+            relatorioUsuario.TotalLista = totalListaLS
+
+            relatoriosSalvos.push(relatorioUsuario)
+
+            //JSON.stringify(localStorage.setItem("", relatoriosSalvos))
+
+            localStorage.setItem("TESTERELATORIO", JSON.stringify(relatoriosSalvos))
+
+            console.log("relatorioUsuario ", JSON.stringify(relatoriosSalvos));
+
+        } else {
+            document.getElementById("idConfirmaCpf").value = ""
+            divConfirmacao.innerHTML = 'Cpf incorreto ou não cadastrado no banco de dados!'
+            document.getElementById("idConfirmaCpf").value = ""
+        }
     }
 }
